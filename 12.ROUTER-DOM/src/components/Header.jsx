89894c2d68e4982be4c93f909c54a475e65b3ea0
MustafaEmirKocyigitt <mustafaemirkocyigit@gmail.com>
2025-06-30 14:13:46 +0300

    bug fixed

--- 12.ROUTER-DOM/src/components/Header.jsx
+++ 12.ROUTER-DOM/src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from "react-router";
 function Header() {
     return (
-        <div className='header'>  
+        <div className='header'>
             <Link className='link' to="/" >Anasayfa</Link>
             <Link className='link' to="/about" >Hakkında</Link>
             <Link className='link' to="/products" >Ürünler</Link>
-            <Link className='link'to="/contact" >İletişim</Link>
+            <Link className='link' to="/contact" >İletişim</Link>
         </div>
     )
 }
--- 12.ROUTER-DOM/src/components/Product.jsx
+++ 12.ROUTER-DOM/src/components/Product.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-function Product({product}) {
-    const { id, name, price, description } = product; // Destructuring the product object
-    const navigate = useNavigate();
+function Product({ product }) {
+  const { id, name, price, description } = product; // Destructuring the product object
+  const navigate = useNavigate();
   return (
     <div>
-            <div>Ürün Detayı</div>
-            <h3>İsim: {name}</h3>
-            <h3>Fiyat: {price}</h3>
-            <h3>Açıklama: {description}</h3>   
-            <button onClick={()=>navigate("/product-details/"+ id)}>Ürün Detayına Git</button>   
-        </div>
+      <div>Ürün Detayı</div>
+      <h3>İsim: {name}</h3>
+      <h3>Fiyat: {price}</h3>
+      <h3>Açıklama: {description}</h3>
+      <button onClick={() => navigate("/products-details/" + id)}>Ürün Detayına Git</button>
+    </div>
   )
 }
 
